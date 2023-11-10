1535b3bac222adffef5cbc25ae8f56b378668794
danielxp88 <danielxp@micorreo.upp.edu.mx>
2023-11-10 10:01:49 -0600

    carrusel

--- centinela/screens/Home.js
+++ centinela/screens/Home.js
@@ -1,12 +1,26 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, Button , Alert} from 'react-native';
-import React, { useState } from 'react'
+import { View, StyleSheet, Image, Alert ,TouchableOpacity} from 'react-native';
+import React from 'react';
+import Swiper from 'react-native-swiper/src';
 
-export default function Home(props) { 
-    return (
-        <View style={styles.container}>
-       
+
+export default function Home(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.carouselContainer}>
+        <Swiper style={styles.wrapper} showsButtons={true}>
+          <View style={styles.slide}>
+            <Image source={require('../assets/prueba1.jpg')} style={styles.image} />
+          </View>
+          <View style={styles.slide}>
+            <Image source={require('../assets/prueba2.jpg')} style={styles.image} />
+          </View>
+          <View style={styles.slide}>
+            <Image source={require('../assets/prueba3.jpg')} style={styles.image} />
+          </View>
+        </Swiper>
+      </View>
       <View style={styles.barra}>
-        <TouchableOpacity onPress={() => Alert.alert('Emergencia','Envaindo tu ubicación')} style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => Alert.alert('Emergencia', 'Enviando tu ubicación')} style={styles.buttonContainer}>
           <Image
             source={require('../assets/advertencia.png')}
             style={styles.buttonImage}
@@ -32,56 +46,52 @@ export default function Home(props) {
         </TouchableOpacity>
       </View>
     </View>
-    );
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor:'white',
-    },
-    barra: {
-      height: 70,
-      width: '100%',
-      backgroundColor: "#365B6D",
-      flexDirection: 'row', // Cambia a 'row' para alinear los elementos horizontalmente
-      justifyContent: 'space-between', // Alinea los elementos en los extremos
-      alignItems: 'center',
-      position: 'absolute',
-      bottom: 0,
-    },
-    buttonContainer: {
-      marginHorizontal:10,
-      width: 45,
-      height: 45,
-      borderRadius: 22.5,
-      backgroundColor: 'white',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    buttonImage: {
-      width: 30,
-      height: 30,
-    },
-    botonImagen: {
-      marginHorizontal:10,
-      width: 150,
-      height: 150,
-      borderRadius: 75,
-      backgroundColor: 'white',
-      justifyContent: 'center',
-      alignItems: 'center',
-     
-    },
-    botonImg: {
-      width: 100,
-      height: 100,
-      
-    },
-    texto:{
-      marginBottom:50,
-    },
-  });
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  carouselContainer: {
+    width: '100%',
+    height: '30%',
+  },
+  wrapper: {},
+  slide: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  barra: {
+    height: 70,
+    width: '100%',
+    backgroundColor: '#365B6D',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+  },
+  buttonContainer: {
+    marginHorizontal: 10,
+    width: 45,
+    height: 45,
+    borderRadius: 22.5,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonImage: {
+    width: 30,
+    height: 30,
+  },
+});
